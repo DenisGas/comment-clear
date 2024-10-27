54c2605f156506e41e64821e6ae142d871e48941
DenisGasilo <dehuc1811@gmail.com>
2024-10-27 21:08:00 +0200

    🔧 Update and configure ESLint & Prettier

--- README.md
+++ README.md
@@ -9,13 +9,15 @@
 CommentClear — is a handy tool for removing comments from your code in the Visual Studio Code editor. The extension supports comment removal for several programming languages, making it an indispensable tool for developers.
 
 ## Preview
+<div align=center>
 
 ![Demo Video](https://github.com/DenisGas/comment-clear/blob/main/assets/demo.gif?raw=true)
 
+</div>
 
 ## Supported file types
 
- comment-clear supports comment removal in the following programming languages:
+comment-clear supports comment removal in the following programming languages:
 
 - JavaScript
 - TypeScript
@@ -30,7 +32,8 @@ CommentClear — is a handy tool for removing comments from your code in the Vis
 ## Features
 
 1. Removing one-line comments: For example, removing comments that start with// (JavaScript, TypeScript) or # (Python).
-2. Removing multi-line comments: Support for removing comments that span multiple lines, such as /* ... */ (JavaScript, CSS) and """ ... """ (Python).
+<!-- prettier-ignore -->
+2. Removing multi-line comments: Support for removing comments that span multiple lines, such as /_ ... _/ (JavaScript, CSS) and """ ... """ (Python).
 3. Context menu: Access the comment deletion function through the right menu of the editor.
 4. Shortcuts: Invoke the command to delete comments using the shortcut Ctrl + Alt + D.
 
--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,27 +1,35 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import prettier from "eslint-plugin-prettier";
+import typescriptEslint from '@typescript-eslint/eslint-plugin';
+import tsParser from '@typescript-eslint/parser';
+import { ESLint } from 'eslint';
+import prettier from 'eslint-plugin-prettier';
 
-export default [{
-    files: ["**/*.ts"],
+
+/** @type {import(ESLint).Linter.Config } */
+export default [
+  {
+    files: ['**/*.ts'],
     plugins: {
-        "@typescript-eslint": typescriptEslint,
-        "prettier": prettier, 
+      '@typescript-eslint': typescriptEslint,
+      prettier: prettier,
     },
     languageOptions: {
-        parser: tsParser,
-        ecmaVersion: 2022,
-        sourceType: "module",
+      parser: tsParser,
+      ecmaVersion: 2022,
+      sourceType: 'module',
     },
     rules: {
-        "prettier/prettier": "error",
-        "@typescript-eslint/naming-convention": ["warn", {
-            selector: "import",
-            format: ["camelCase", "PascalCase"],
-        }],
-        curly: "warn",
-        eqeqeq: "warn",
-        "no-throw-literal": "warn",
-        semi: "warn",
+      'prettier/prettier': 'error',
+      '@typescript-eslint/naming-convention': [
+        'warn',
+        {
+          selector: 'import',  
+          format: ['camelCase', 'PascalCase'],
+        },
+      ],
+      curly: 'warn',
+      eqeqeq: 'warn',
+      'no-throw-literal': 'warn',
+      semi: 'warn',
     },
-}];
+  },
+];
